86c1e3c5123110308cbbad6839a674c804177930
Joey Zheng <joey.z.rp@gmail.com>
2019-04-04 16:43:01 +1000

    Create clear session release

--- src/content.js
+++ src/content.js
@@ -1,21 +1,3 @@
-let store = {};
-
-function injectScript(file, node) {
-    var th = document.getElementsByTagName(node)[0];
-    var s = document.createElement('script');
-    s.setAttribute('type', 'text/javascript');
-    s.setAttribute('src', file);
-    th.appendChild(s);
-}
-injectScript( chrome.extension.getURL('inject.js'), 'html');
-
-var storageHandler = function(e) {
-    chrome.runtime.sendMessage({sessionStorage, store});
-    store = JSON.parse(JSON.stringify(sessionStorage));
-};
-
-document.addEventListener("itemInserted", storageHandler, false);
-
 chrome.runtime.onMessage.addListener((request) => {
     if (request.text === "clearSessionStorage") {
         sessionStorage.clear();
