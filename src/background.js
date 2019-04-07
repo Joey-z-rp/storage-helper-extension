0dccbf1e437f078a8258f0d7c5358b91542a81c9
Joey Zheng <joey.z.rp@gmail.com>
2019-04-07 19:45:38 +1000

    Add notifications

--- src/background.js
+++ src/background.js
@@ -4,3 +4,17 @@ chrome.commands.onCommand.addListener((command) => {
         tabs => chrome.tabs.sendMessage(tabs[0].id, { command }),
     );
 });
+
+chrome.runtime.onMessage.addListener((request) => {
+    if (request.type === 'notification') sendNotification(request.options);
+});
+
+function sendNotification(options) {
+    const mergedOptions = {
+        type: 'basic',
+        title: 'Storage Status:',
+        iconUrl: './icons/storage.png',
+        ...options,
+    };
+    chrome.notifications.create(mergedOptions);
+}
--- src/content.js
+++ src/content.js
@@ -12,21 +12,41 @@ chrome.runtime.onMessage.addListener((request) => {
 function clearSessionStorage() {
     sessionStorage.clear();
     location.reload();
+
+    sendNotification({
+        message: 'Session storage cleared.',
+        iconUrl: './icons/clear.png',
+    });
 }
 
 function clearLocalStorage() {
     localStorage.clear();
     location.reload();
+
+    sendNotification({
+        message: 'Local storage cleared.',
+        iconUrl: './icons/clear.png',
+    });
 }
 
 function saveSessionStorage() {
-    chrome.storage.local.set({ session: Object.entries(sessionStorage) });
+    chrome.storage.local.set({ session: Object.entries(sessionStorage) }, () => {
+        sendNotification({
+            message: 'Session storage saved.',
+            iconUrl: './icons/save.png',
+        });
+    });
 }
 
 function loadSessionStorage() {
     chrome.storage.local.get('session', (item) => {
         item.session.forEach((entry) => {
             sessionStorage.setItem(entry[0], entry[1]);
+        });
+
+        sendNotification({
+            message: 'Session storage loaded.',
+            iconUrl: './icons/load.png',
         });
     });
 }
@@ -41,6 +61,10 @@ function loadLocalStorage() {
             localStorage.setItem(entry[0], entry[1]);
         });
     });
+}
+
+function sendNotification(options) {
+    chrome.runtime.sendMessage({ options, type: 'notification' });
 }
 
 function executeCommand(command) {
